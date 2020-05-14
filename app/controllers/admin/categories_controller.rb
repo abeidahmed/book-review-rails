c9ebe2928ccca9ae4bf5df4e4af4b535c567312d
abeidahmed <abeidahmed92@gmail.com>
2020-05-14 21:40:34 +0530

    Use dynamic url

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_param)
     if @category.save
-      redirect_to "/admin/categories"
+      redirect_to admin_categories_path
       flash.now[:success] = "Successfully created a new category #{@category.title}."
     else
       render "new"
