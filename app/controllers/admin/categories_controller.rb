e7fd1bef246195fc35a6b25575437b406e39cce7
abeidahmed <abeidahmed92@gmail.com>
2020-05-14 21:22:14 +0530

    Create a functional category add form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,25 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_param)
+    if @category.save
+      redirect_to "/admin/categories"
+      flash.now[:success] = "Successfully created a new category #{@category.title}."
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_param
+    params.require(:category).permit(:title, :description)
   end
 end
