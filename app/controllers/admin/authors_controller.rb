dcc08e3d17d4ece769e3ccdd580c7533d48c6339
abeidahmed <abeidahmed92@gmail.com>
2020-05-15 20:04:15 +0530

    Create author add page and edit page

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -1,4 +1,39 @@
 class Admin::AuthorsController < ApplicationController
   def index
+    @authors = Author.order_by_date
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to admin_authors_path
+      flash.now[:success] = "Successfully created a new author: #{@author.name}"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update_attributes(author_params)
+      redirect_to admin_authors_path
+      flash.now[:success] = "Successfully updated author #{@author.name}"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :description)
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  scope :order_by_date, -> { order(created_at: :desc) }
 end
