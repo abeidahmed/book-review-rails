7995aab94b3ddb058031722d9551c8c0f660cffe
abeidahmed <abeidahmed92@gmail.com>
2020-05-15 19:27:57 +0530

    Implement category editing

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -17,6 +17,20 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_param)
+      redirect_to admin_categories_path
+      flash.now[:success] = "Successfully updated category #{@category.title}"
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def category_param
