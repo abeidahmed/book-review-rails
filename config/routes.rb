3e8f1768e4a38536a6c5fe767f18cf47954357f4
abeidahmed <abeidahmed92@gmail.com>
2020-05-15 19:39:36 +0530

    Create author controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
+  
   resources :users
   resources :sessions
 
   namespace :admin do
+    resources :authors
     resources :books
     resources :categories
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_150624) do
+ActiveRecord::Schema.define(version: 2020_05_15_140735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
