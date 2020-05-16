0b2fc5accd53375a85ba36f5ac52bb5b3b6a3c8c
abeidahmed <abeidahmed92@gmail.com>
2020-05-16 20:01:37 +0530

    Add dependent destroy

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -33,6 +33,12 @@ class Admin::BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to admin_books_path
+  end
+
   private
 
   def book_params
